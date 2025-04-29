2997ffd8cdb8d28aea2863b48d7c51f19b7ad2f9
Zsolt Kacsor <katsa007@gmail.com>
2025-04-29 15:03:56 +0200

    Shadcn előtti állapot

---
+++ app/dashboard/components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col items-center justify-center w-full h-screen'>
+        <h1 className='text-white'>HERO</h1>
+    </div>
+  )
+}
+
+export default Hero
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import DashBoardOverview from "./dashboard/components/DashBoardOverview";
+import Hero from "./dashboard/components/Hero";
+import SideBar from "./dashboard/components/SideBar";
 
 
 export default function Home() {
   return (
    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <SideBar />
    <DashBoardOverview />
+   <Hero />
    </main>
   );
 }
